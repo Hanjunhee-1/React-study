ceafa1908c66175a0654ddce6060dd4b729aede7
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-29 18:09:26 +0900

    Comment.jsx, CommentList.jsx 코드 수정

--- react-study/src/05_실습_댓글컴포넌트작성해보기/Comment.jsx
+++ react-study/src/05_실습_댓글컴포넌트작성해보기/Comment.jsx
@@ -1,7 +1,52 @@
+const styles = {
+    wrapper: {
+        margin: 8,
+        padding: 8,
+        display: "flex",
+        flexDirection: "row",
+        border: "1px solid gray",
+        borderRadius: 16,
+    },
+    imageContainer: {},
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    contentContainer: {
+        marginLeft: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    nameText: {
+        color: "black",
+        fontSize: "16",
+        fontWeight: "bold",
+    },
+    commentText: {
+        color: "black",
+        fontSize: 16,
+    },
+}
+
 function Comment(props) {
     return (
-        <div>
-            <h1>댓글 컴포넌트입니다.</h1>
+        <div style={styles.wrapper}>
+            <div style={styles.imageContainer}>
+                <img 
+                    src="http://internetlawclinic.org/common/img/default_profile.png"
+                    alt="TEST"
+                    style={styles.image}
+                />
+            </div>
+
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>{props.name}</span>
+                <span style={styles.commentText}>
+                    {props.content}
+                </span>
+            </div>
         </div>
     )
 }
--- react-study/src/05_실습_댓글컴포넌트작성해보기/CommentList.jsx
+++ react-study/src/05_실습_댓글컴포넌트작성해보기/CommentList.jsx
@@ -1,9 +1,30 @@
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "익명1",
+        content: "React 배우다보니 할만하네요"
+    },
+    {
+        name: "익명2",
+        content: "엄청 어려운 줄 알았는데 그냥 할만해요"
+    },
+    {
+        name: "익명3",
+        content: "하기 전부터 겁을 먹으면 안되는 이유"
+    }
+]
+
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            {
+                comments.map((comment) => {
+                    return (
+                        <Comment name={comment.name} content={comment.content} />
+                    )
+                })
+            }
         </div>
     )
 }
