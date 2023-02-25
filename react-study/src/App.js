8b470f2ed697717dd58043aa2280d70d9b719ae6
Hanjunhee-1 <hanjuni857@naver.com>
2023-02-26 03:54:51 +0900

    App.js 파일 수정 및 index.js 파일 수정

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import styled from 'styled-components';
+import MainPage from './17_프로젝트/component/page/MainPage';
+import PostWritePage from './17_프로젝트/component/page/PostWritePage';
+import PostViewPage from './17_프로젝트/component/page/PostViewPage';
 
-function App() {
+const MainTitleText = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+`
+
+export default function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <MainTitleText>나의 미니 블로그</MainTitleText>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path='post-write' element={<PostWritePage />} />
+        <Route path='post/:postId' element={<PostViewPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
--- react-study/src/index.js
+++ react-study/src/index.js
@@ -4,29 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './01_실습_JSX코드작성/Library';
-import ConfirmDialog from './02_실습_Element알아보기/ConfirmDialog';
-import Clock from './03_실습_시계만들기/Clock';
-import Market from './04_실습_createElement사용해보기/Market';
-import CommentList from './05_실습_댓글컴포넌트작성해보기/CommentList';
-import NotificationList from './06_실습_State사용해보기/NotificationList';
-import Counter from './07_실습_useState와useEffect사용해보기/Counter';
-import Accomodate from './08_실습_Hooks사용해보기/Accomodate';
-import ConfirmButton from './09_실습_클릭이벤트처리/ConfirmButton';
-import LandingPage from './10_실습_로그인여부툴바만들기/LandingPage';
-import AttendanceBook from './11_실습_출석부/AttendanceBook';
-import SignUp from './12_실습_사용자정보입력받기/SignUp';
-import Calculator from './13_실습_섭씨온도와화씨온도표시하기/Calculator';
-import ProfileCard from './14_실습_Card컴포넌트만들기/ProfileCard';
-import DarkOrLight from './15_실습_Context를활용한테마변경기능/DarkOrLight';
-import Blocks from './16_실습_StyledComponents사용하기/Blocks';
+// import Library from './01_실습_JSX코드작성/Library';
+// import ConfirmDialog from './02_실습_Element알아보기/ConfirmDialog';
+// import Clock from './03_실습_시계만들기/Clock';
+// import Market from './04_실습_createElement사용해보기/Market';
+// import CommentList from './05_실습_댓글컴포넌트작성해보기/CommentList';
+// import NotificationList from './06_실습_State사용해보기/NotificationList';
+// import Counter from './07_실습_useState와useEffect사용해보기/Counter';
+// import Accomodate from './08_실습_Hooks사용해보기/Accomodate';
+// import ConfirmButton from './09_실습_클릭이벤트처리/ConfirmButton';
+// import LandingPage from './10_실습_로그인여부툴바만들기/LandingPage';
+// import AttendanceBook from './11_실습_출석부/AttendanceBook';
+// import SignUp from './12_실습_사용자정보입력받기/SignUp';
+// import Calculator from './13_실습_섭씨온도와화씨온도표시하기/Calculator';
+// import ProfileCard from './14_실습_Card컴포넌트만들기/ProfileCard';
+// import DarkOrLight from './15_실습_Context를활용한테마변경기능/DarkOrLight';
+// import Blocks from './16_실습_StyledComponents사용하기/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Blocks />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
 
